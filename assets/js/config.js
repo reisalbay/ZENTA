71e7989b089ec8f8ee1af13cdbe135aae6dc93b9
sahibindenihale <aviserent@gmail.com>
2026-03-14 02:40:23 +0300

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,9 @@
+window.SITE_CONFIG = {
+  companyName: 'ZENTA FİLO',
+  phoneDisplay: '0530 763 03 19',
+  phoneRaw: '905307630319',
+  whatsappRaw: '905307630319',
+  whatsappMessage: 'Merhaba, Zenta Filo üzerinden araç rezervasyonu yapmak istiyorum.',
+  locations: ['İstanbul Havalimanı', 'Sabiha Gökçen', 'Taksim', 'Kadıköy', 'Beşiktaş', 'Bursa Merkez'],
+  workingHours: '7/24 rezervasyon ve teslimat desteği'
+};
