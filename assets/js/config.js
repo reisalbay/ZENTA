ed4a0196d14aef87b1338d62f863f3663fa3ba69
reisalbay <aviserent@gmail.com>
2026-05-02 11:43:57 +0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,8 +1,8 @@
 window.SITE_CONFIG = {
   companyName: 'ZENTA FİLO',
-  phoneDisplay: '905541197088',
-  phoneRaw: '905541197088',
-  whatsappRaw: '905532651080',
+  phoneDisplay: '905078162654',
+  phoneRaw: '905078162654',
+  whatsappRaw: '905521211575',
   whatsappMessage: 'Merhaba, Zenta Filo üzerinden araç rezervasyonu yapmak istiyorum.',
   locations: ['İstanbul Havalimanı', 'Sabiha Gökçen', 'Taksim', 'Kadıköy', 'Beşiktaş', 'Bursa Merkez'],
   workingHours: '7/24 rezervasyon ve teslimat desteği'
